cdc7b39308d3ec3b9e04700a9877352cbb921814
Bob Callaway <bcallaway@google.com>
2022-12-01 03:39:21 -0500

    initial restore impl

---
+++ restore/go.mod
@@ -0,0 +1,3 @@
+module github.com/naveensrinivasan/rekor-phren/restore
+
+go 1.19
---
+++ restore/invoke.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	http.HandleFunc("/", scriptHandler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	// Start HTTP server.
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func scriptHandler(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.CommandContext(r.Context(), "/bin/bash", "restore.sh")
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	if err != nil {
+		w.WriteHeader(500)
+	}
+	_, _ = w.Write(out)
+}
