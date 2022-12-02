9224438ff64b38efa59b59c988a367c60af1395c
Bob Callaway <bcallaway@google.com>
2022-12-03 08:37:35 +0900

    block on restore finishing and be more verbose about actions taken

--- restore/restore.sh
+++ restore/restore.sh
@@ -7,11 +7,10 @@ SOURCE_SQL_INSTANCE_NAME="rekor-dev"
 TARGET_PROJECT_NAME="openssf"
 TARGET_SQL_INSTANCE_NAME="rekor-5-7"
 
-# Need to get the list of recent backups
 BACKUP_LIST=$(mktemp)
 
 # Fetches the list of backups on the source instance
-echo "Fetching list of backups available from the source instance..."
+echo "Fetching list of backups available from the source SQL instance '$SOURCE_SQL_INSTANCE_NAME' in project '$SOURCE_PROJECT_NAME' ..."
 curl -f -s -o $BACKUP_LIST -X GET -H "Authorization: Bearer $(gcloud auth print-access-token)" "https://sqladmin.googleapis.com/v1/projects/${SOURCE_PROJECT_NAME}/instances/${SOURCE_SQL_INSTANCE_NAME}/backupRuns"
 
 # Calculates the backup run ID for the most recent successful backup
@@ -19,7 +18,7 @@ BACKUP_RUN_ID=$(jq -r '[.items[] | select(.status | contains("SUCCESSFUL"))] | s
 
 # Prints the time the backup was taken
 START_TIME_BACKUP_RUN_ID=$(jq -r '[.items[] | select(.id | contains("'$BACKUP_RUN_ID'"))] | .[0].startTime ' $BACKUP_LIST)
-echo "Attempting to restore backup run $BACKUP_RUN_ID from $START_TIME_BACKUP_RUN_ID..."
+echo "Attempting to restore backup run $BACKUP_RUN_ID from $START_TIME_BACKUP_RUN_ID into target SQL instance '$TARGET_SQL_INSTANCE_NAME' in project '$TARGET_PROJECT_NAME' ..."
 
 generate_restore_request()
 {
@@ -35,5 +34,10 @@ generate_restore_request()
 EOF
 }
 
+BACKUP_OPERATION_INFO=$(mktemp)
 # Sends the restore from backup request to the target instance within the target project
-curl -f -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json; charset=utf-8" --data "$(generate_restore_request)" "https://sqladmin.googleapis.com/v1/projects/${TARGET_PROJECT_NAME}/instances/${TARGET_SQL_INSTANCE_NAME}/restoreBackup"
+curl -f -s -o $BACKUP_OPERATION_INFO -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" -H "Content-Type: application/json; charset=utf-8" --data "$(generate_restore_request)" "https://sqladmin.googleapis.com/v1/projects/${TARGET_PROJECT_NAME}/instances/${TARGET_SQL_INSTANCE_NAME}/restoreBackup"
+
+# Block returning from this script until operation completes
+OPERATION_NAME=$(jq -r '.name' $BACKUP_OPERATION_INFO)
+gcloud sql operations wait "$OPERATION_NAME" --timeout=unlimited --project=$TARGET_PROJECT_NAME
